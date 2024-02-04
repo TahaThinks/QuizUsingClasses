fab22254653751e70d799f9a0e6143a0a8350786
Taha Ahmed Abdelazim Hussein <71207531+TahaThinks@users.noreply.github.com>
2024-02-04 19:22:50 +0400

    Refine Code

--- data.py
+++ data.py
@@ -1,9 +1,3 @@
-class Question:
-    def __init__(self, text, answer):
-        self.question = text
-        self.answer = answer
-
-
 
 question_data = [
 {"text": "A slug's blood is green.", "answer": "True"},
