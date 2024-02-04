048df06892128ef6c294bd2ead8e4fe0549c35fb
Taha Ahmed Abdelazim Hussein <71207531+TahaThinks@users.noreply.github.com>
2024-02-04 21:24:14 +0400

    Create Check Answer Method

--- quiz_brain.py
+++ quiz_brain.py
@@ -3,6 +3,7 @@ class QuizBrain:
     def __init__(self, q_list):
         self.question_number = 0
         self.question_list = q_list
+        self.score = 0
 
     def still_has_questions(self):
         return self.question_number < len(self.question_list)
@@ -10,4 +11,8 @@ class QuizBrain:
     def next_question(self):
         current_question = self.question_list[self.question_number]
         self.question_number += 1
-        received_answer = input(f"Q.{self.question_number} {current_question.text} (True/False)? ").lower()
+        user_answer = input(f"Q.{self.question_number} {current_question.text} (True/False)? ").lower()
+        self.check_answer(user_answer,current_question.answer)
+
+    def check_answer(self,user_answer,correct_answer):
+        pass
