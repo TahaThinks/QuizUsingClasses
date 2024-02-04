b7bd4c23e0adb667a585ddea70fabc50978b4b25
Taha Ahmed Abdelazim Hussein <71207531+TahaThinks@users.noreply.github.com>
2024-02-04 21:28:22 +0400

    Add check_answer() logic

--- quiz_brain.py
+++ quiz_brain.py
@@ -11,8 +11,15 @@ class QuizBrain:
     def next_question(self):
         current_question = self.question_list[self.question_number]
         self.question_number += 1
-        user_answer = input(f"Q.{self.question_number} {current_question.text} (True/False)? ").lower()
+        user_answer = input(f"Q.{self.question_number} {current_question.text} (True/False)? ")
         self.check_answer(user_answer,current_question.answer)
 
     def check_answer(self,user_answer,correct_answer):
-        pass
+        if user_answer.lower() == correct_answer.lower():
+            self.score += 1 
+            print("You go it right!")
+        else:
+            print("That's wrong.")
+        
+        print(f"The Correct answer is {correct_answer}")
+        print(f"Your current score is: {self.score}/{self.question_number}")
