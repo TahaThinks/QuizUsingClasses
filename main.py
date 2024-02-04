cc73937b669bd372a11db01e9a25b5a46460f0d9
Taha Ahmed Abdelazim Hussein <71207531+TahaThinks@users.noreply.github.com>
2024-02-04 19:13:14 +0400

    Create Follow Method

--- main.py
+++ main.py
@@ -3,10 +3,18 @@ class User:
         self.id = user_id
         self.username = username
         self.followers = 0
+        self.following = 0
+
+    def follow(self, user):
+        user.followers += 1 #---> Increase the user you will follow by 1
+        self.following += 1 #--> Increase the following of your account by 1
+
 
 
 user_1 = User("001", "angela")
 user_2 = User("002", "taha")
 
-print(user_1.username)
-print(user_2.username)
+user_1.follow(user_2)
+
+print(user_1.following)
+print(user_2.followers)
